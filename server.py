c45a382c84fdd616b6513ac30a735b9b00240f8e
23hh <93519176+23hh@users.noreply.github.com>
2021-11-05 10:06:31 +0900

    상세페이지 연결

---
+++ server.py
@@ -0,0 +1,108 @@
+from pymongo import MongoClient
+import jwt
+import datetime
+import hashlib
+import requests
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from werkzeug.utils import secure_filename
+from datetime import datetime, timedelta
+
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+app.config['UPLOAD_FOLDER'] = "./static/profile_pics"
+
+SECRET_KEY = 'SPARTA'
+
+client = MongoClient('localhost', 27017)
+db = client.dbsparta_plus
+
+
+@app.route('/')
+def home():
+    token_receive = request.cookies.get('mytoken')
+    movies = list(db.movies.find({}, {'_id': False}))
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+
+        return render_template('main.html', movies=movies)
+    except jwt.ExpiredSignatureError:
+        return redirect(url_for("login", msg="로그인 시간이 만료되었습니다."))
+    except jwt.exceptions.DecodeError:
+        return redirect(url_for("login", msg="로그인 정보가 존재하지 않습니다."))
+
+
+@app.route('/login')
+def login():
+    msg = request.args.get("msg")
+    return render_template('hlogin.html', msg=msg)
+
+@app.route('/detail/<name>')
+def detail(name): # 영화목록 api
+    movies = db.movies.find_one({'name':name}, {'_id':False})
+    return render_template('detail.html', movies=movies)
+
+
+
+@app.route('/sign_in', methods=['POST'])
+def sign_in():
+    # 로그인
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+
+    pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    result = db.users.find_one({'username': username_receive, 'password': pw_hash})
+
+    if result is not None:
+        payload = {
+         'id': username_receive,
+         'exp': datetime.utcnow() + timedelta(seconds=60*60)  # 로그인 24시간 유지
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
+
+        return jsonify({'result': 'success', 'token': token})
+    # 찾지 못하면
+    else:
+        return jsonify({'result': 'fail', 'msg': '아이디/비밀번호가 일치하지 않습니다.'})
+
+
+@app.route('/sign_up/save', methods=['POST'])
+def sign_up():
+    # 회원가입
+    username_receive = request.form['username_give']
+    password_receive = request.form['password_give']
+    email_receive = request.form['email_give']
+    password_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
+    # DB에 저장
+    doc = {
+        "username": username_receive,  # 아이디
+        "password": password_hash,  # 비밀번호
+        "email": email_receive,
+        "profile_name": username_receive,  # 프로필 이름 기본값은 아이디
+    }
+    db.users.insert_one(doc)
+    return jsonify({'result': 'success'})
+
+
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup(): # 아이디 중복확인
+    username_receive = request.form['username_give']
+    exists = bool(db.users.find_one({"username": username_receive}))
+    return jsonify({'result': 'success', 'exists': exists})
+
+
+# @app.route('/detail')
+# def get_movies():
+#     get = request.args.get('movie',type=str)
+#     movies = db.movies.find_one({'name':get}, {'_id':False})
+#     return render_template('detail.html', movies=movies)
+
+
+# @app.route('/api/search')
+# def search_movies():
+#     searchmovie = list(db.movies.find({}, {'_id':False}))
+#     return render_template("detail.html", searchmovie=searchmovie)
+
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', port=5000, debug=True)
